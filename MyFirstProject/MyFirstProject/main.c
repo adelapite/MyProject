149debe2ec9ba131d6dbdd8649a86ea41414838b
adelapite <adelapite@yahoo.co.uk>
2014-06-19 13:08:35 +0100

    MyFirstProject/MyFirstProject.xcodeproj/project..pbxproj

---
+++ MyFirstProject/MyFirstProject/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  MyFirstProject
+//
+//  Created by Ade on 19/06/2014.
+//  Copyright (c) 2014 Ade. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
